d62c5399756cc99462a3db59e5326a1f8b4c8a59
Leo <workspace2707@yandex.ru>
2024-01-27 20:24:58 +0300

    created adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
+/* SHOW/HIDE MOBILE MENU */
+
+// const burgerButton = document.getElementById("burgerButton");
+// const navMobile = document.querySelector(".header__grid_nav_mobile");
+
+// burgerButton.addEventListener("click", function () {
+//   navMobile.classList.toggle("showMenu");
+//   navMobile.classList.toggle("hideMenu");
+// });
+
 /* INIT AOS */
 
 AOS.init();
